e778c914fecabf3be4b0b1eccbc63e61f6d7b35c
Enrique <elaborao@hotmail.com>
2022-03-27 00:45:42 -0300

    [SERVER] added cors

--- server/app.js
+++ server/app.js
@@ -2,11 +2,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { getCart, postCart } = require("./resources/cart");
 const { getTest, postTest } = require("./resources/test");
+const cors = require("cors");
 
 require("dotenv").config();
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 app.get("/cart", getCart);
 app.post("/cart", postCart);
