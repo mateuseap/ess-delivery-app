07f6fe7b21d3485567a143eabd60d80a13a39d46
Guilherme Morone Araujo <guimorone@gmail.com>
2022-03-28 20:16:02 -0300

    [SERVER] pegar usuários do banco funcionando com sucesso. Métodos da classe atualizados

--- server/resources/user.js
+++ server/resources/user.js
@@ -1,14 +1,11 @@
-const ManipulateDatabase = require("../utils/db");
+const { ManipulateDatabase } = require("../utils/db");
 
 exports.getUser = async (req, res) => {
   try {
-    const db = new ManipulateDatabase("users", "read");
-    const users = db.getFileContent().users;
+    const table = new ManipulateDatabase("users", "read");
+    const users = table.getFileContent().users;
     res.status(200).send(
-      JSON.stringify({
-        name: "Felipe Gonçalves",
-        id: 5,
-      })
+      JSON.stringify(users)
     );
   } catch (err) {
     res.status(400).send(err);
--- server/utils/db.js
+++ server/utils/db.js
@@ -1,29 +1,43 @@
 const fs = require("fs");
+// ele vê o caminho a partir da raiz do projeto
+const path = './server/data/';
 
 exports.ManipulateDatabase = class {
-  constructor(dbName, mode = "append", content = "") {
-    this.dbName = dbName + ".json";
+  constructor(tableName, mode = "read", content = "") {
+    this.tableName = path + tableName + ".json";
     this.content = JSON.stringify(content);
     switch (mode) {
       case "write":
-        fs.writeFile(this.dbName, this.content, (err) => {
-          if (err) throw err;
-        });
+        this.#write();
         break;
       case "append":
-        fs.appendFile(this.dbName, this.content, (err) => {
-          if (err) throw err;
-        });
+        this.#append();
         break;
       case "read":
-        fs.readFile(this.dbName, "utf-8", (err, data) => {
-          if (err) throw err;
-          this.response = JSON.parse(data);
-        });
+        this.#read();
         break;
       default:
         console.log(mode);
     }
+  }
+
+  #read() {
+    this.response = JSON.parse(fs.readFileSync(this.tableName, "utf8", (err) => {
+      if (err) throw err;
+    }));
+  }
+
+  #write(obj = this.content) {
+    // sobrescreve tudo
+    fs.writeFileSync(this.tableName, obj, (err) => {
+      if (err) throw err;
+    });
+  }
+
+  #append() {
+    this.read();
+    this.response.push(this.content);
+    this.write(this.response);
   }
 
   getFileContent() {
