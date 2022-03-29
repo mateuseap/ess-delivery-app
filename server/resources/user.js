3ec94280ec2fbbcab1b13ce9cb8b3bea160d91a3
Guilherme Morone <guilherme.morone@oncase.com.br>
2022-03-29 09:29:05 -0300

    [SERVER] atributos privados declarados na classe ManipulateDatabase

--- server/resources/user.js
+++ server/resources/user.js
@@ -3,7 +3,7 @@ const { ManipulateDatabase } = require("../utils/db");
 exports.getUser = async (req, res) => {
   try {
     const table = new ManipulateDatabase("users", "read");
-    res.status(200).send(JSON.stringify(table.array));
+    res.status(200).send(JSON.stringify(table.getArray()));
   } catch (err) {
     res.status(400).send(err);
   }
--- server/utils/db.js
+++ server/utils/db.js
@@ -5,25 +5,30 @@ const jsonQuery = require("json-query");
 const path = "./data/";
 
 exports.ManipulateDatabase = class {
+  // private
+  #tableName;
+  #document;
+  #key;
+
   constructor(tableName) {
-    this.tableName = path + tableName + ".json";
+    this.#tableName = path + tableName + ".json";
     this.read();
   }
 
   read() {
-    this.document = JSON.parse(
-      fs.readFileSync(this.tableName, "utf8", (err) => {
+    this.#document = JSON.parse(
+      fs.readFileSync(this.#tableName, "utf8", (err) => {
         if (err) throw err;
       })
     );
-    this.key = Object.keys(this.document)[0];
+    this.#key = Object.keys(this.#document)[0];
   }
 
   write(obj) {
     // sobrescreve tudo
     // aqui seria pra remover conteúdo ou atualizar
-    // IMPORTANTE => obj é do tipo this.document (não é só o array)
-    fs.writeFileSync(this.tableName, JSON.stringify(obj), (err) => {
+    // IMPORTANTE => obj é do tipo this.#document (não é só o array)
+    fs.writeFileSync(this.#tableName, JSON.stringify(obj), (err) => {
       if (err) throw err;
     });
     this.read();
@@ -32,7 +37,7 @@ exports.ManipulateDatabase = class {
   // como é arquivo JSON, temos que fazer o append dessa forma
   append(content) {
     // tem que puxar pra dentro do array, e não pro objeto
-    this.document[this.key].push(content);
+    this.#document[this.#key].push(content);
     this.updateChanges();
   }
 
@@ -41,29 +46,29 @@ exports.ManipulateDatabase = class {
   // se replaceItems for vazio => só vai ter delete
   deleteOrReplace(startIndex, deleteCount = 0, ...replaceItems) {
     if (deleteCount || replaceItems.length) {
-      this.document[this.key].splice(startIndex, deleteCount, replaceItems);
+      this.#document[this.#key].splice(startIndex, deleteCount, replaceItems);
       this.updateChanges();
     }
   }
 
   query(match) {
     // Comandos para fazer a query podem ser vistos abaixo:
-    // não precisa adicionar o nome do array (a concatenação com o this.key já faz isso)
+    // não precisa adicionar o nome do array (a concatenação com o this.#key já faz isso)
     // https://www.npmjs.com/package/json-query
-    return jsonQuery(this.key + match, { data: this.document }).value;
+    return jsonQuery(this.#key + match, { data: this.#document }).value;
   }
 
   updateChanges() {
-    this.write(this.document);
+    this.write(this.#document);
   }
 
   getArray() {
-    return this.document[this.key];
+    return this.#document[this.#key];
   }
 
   // dev tools
   printArrayCollection() {
-    this.document[this.key].forEach((element) => {
+    this.#document[this.#key].forEach((element) => {
       if (Array.isArray(element) && element.length) {
         element.forEach((e) => {
           // para não ter quebra de linha
