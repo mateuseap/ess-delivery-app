a01d36f27f31d03f30a335f5a935c71a7adf5d03
Guilherme Morone Araujo <guimorone@gmail.com>
2022-04-06 21:43:18 -0300

    [CLIENT] Font-family global alterada e mudanças de estilização no header

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-import { HomeStyle, TextStyle } from "./styles";
+import { TextStyle } from "./styles";
 
 import { Link } from "react-router-dom";
-import { Card, Button } from "react-bootstrap";
+import { Card, Button, Container, Row, Col } from "react-bootstrap";
 
 import { connect } from "react-redux";
 import { Creators as RestaurantsCreator } from "../../store/ducks/restaurants";
@@ -10,47 +10,51 @@ import { Creators as RestaurantsCreator } from "../../store/ducks/restaurants";
 class Home extends Component {
   getCardStyle(element) {
     return (
-      <Card
-        key={element.restId}
-        style={{
-          borderRadius: "50px",
-          alignItems: "center",
-          width: "350px",
-          background: "rgba(245, 245, 245, 0.8)",
-          listStyle: "none",
-          flexBasis: "auto",
-        }}
-        className="m-4"
-      >
-        <Card.Img
-          variant="top"
-          src={element.photo}
+      <Col>
+        <Card
+          key={element.restId}
           style={{
             borderRadius: "50px",
+            alignItems: "center",
             width: "350px",
-            height: "263px",
+            background: "rgba(245, 245, 245, 0.8)",
+            listStyle: "none",
+            flexBasis: "auto",
           }}
-        />
-        <Card.Body
-          style={{
-            textAlign: "center",
-          }}
+          className="m-4"
         >
-          <Card.Title style={{ color: "#E83A14" }}>
-            {element.restName}
-          </Card.Title>
-          <Card.Text style={{ color: "#1B1A17" }}>{element.foodName}</Card.Text>
-          <Card.Text style={{ color: "#05595B" }}>
-            {element.description}
-          </Card.Text>
-        </Card.Body>
-        {/* Quando apertar esse botão, o usuário deve ser redirecionado a tela de fazer pedidos com esse restaurante selecionado */}
-        <Link to={`/fazer_pedido?restaurant_id=${element.restId}`}>
-          <Button variant="success" className="m-2">
-            PEÇA JÁ!
-          </Button>
-        </Link>
-      </Card>
+          <Card.Img
+            variant="top"
+            src={element.photo}
+            style={{
+              borderRadius: "50px",
+              width: "350px",
+              height: "263px",
+            }}
+          />
+          <Card.Body
+            style={{
+              textAlign: "center",
+            }}
+          >
+            <Card.Title style={{ color: "#E83A14" }}>
+              {element.restName}
+            </Card.Title>
+            <Card.Text style={{ color: "#1B1A17" }}>
+              {element.foodName}
+            </Card.Text>
+            <Card.Text style={{ color: "#05595B" }}>
+              {element.description}
+            </Card.Text>
+          </Card.Body>
+          {/* Quando apertar esse botão, o usuário deve ser redirecionado a tela de fazer pedidos com esse restaurante selecionado */}
+          <Link to={`/fazer_pedido?restaurant_id=${element.restId}`}>
+            <Button variant="success" className="m-2">
+              PEÇA JÁ!
+            </Button>
+          </Link>
+        </Card>
+      </Col>
     );
   }
 
@@ -62,21 +66,28 @@ class Home extends Component {
 
   render() {
     return (
-      <HomeStyle>
-        <TextStyle className="m-1">{this.h1Text}</TextStyle>
-        {this.props.restaurants.data.map((restaurant) =>
-          this.getCardStyle({
-            restId: restaurant.id,
-            photo: restaurant.menu.options[restaurant.menu.destaqueIndex].photo,
-            restName: restaurant.name,
-            foodName:
-              restaurant.menu.options[restaurant.menu.destaqueIndex].name,
-            description:
-              restaurant.menu.options[restaurant.menu.destaqueIndex]
-                .description,
-          })
-        )}
-      </HomeStyle>
+      <Container>
+        <Row className="justify-content-md-center">
+          <Col>
+            <TextStyle className="mt-3">{this.h1Text}</TextStyle>
+          </Col>
+        </Row>
+        <Row className="justify-content-md-center">
+          {this.props.restaurants.data.map((restaurant) =>
+            this.getCardStyle({
+              restId: restaurant.id,
+              photo:
+                restaurant.menu.options[restaurant.menu.destaqueIndex].photo,
+              restName: restaurant.name,
+              foodName:
+                restaurant.menu.options[restaurant.menu.destaqueIndex].name,
+              description:
+                restaurant.menu.options[restaurant.menu.destaqueIndex]
+                  .description,
+            })
+          )}
+        </Row>
+      </Container>
     );
   }
 }
--- client/src/components/Home/styles.js
+++ client/src/components/Home/styles.js
@@ -1,17 +1,10 @@
 import styled from "styled-components";
 
-export const HomeStyle = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  flex-wrap: wrap;
-`;
-
 export const TextStyle = styled.h1`
-  text-align: "center";
+  display: flex;
+  font-family: Helvetica;
+  justify-content: center;
   font-weight: 500;
-  font-size: "2.5rem";
   line-height: "94px";
   color: "#630606";
 `;
