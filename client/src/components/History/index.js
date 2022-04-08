7bf89ed9d6cc68d729d816d0d4e8707bb979d3c0
Guilherme Morone Araujo <guimorone@gmail.com>
2022-04-07 22:39:04 -0300

    [CLIENT] página de histórico iniciada

--- client/src/components/History/index.js
+++ client/src/components/History/index.js
@@ -1,16 +1,98 @@
 import React, { Component } from "react";
+import {
+  RateLabel,
+  BorderText,
+  PageStyle,
+  MainDiv,
+  TableStyle,
+  TableBodyStyle,
+  ActionButtonsStyle,
+} from "./styles";
 
 import { connect } from "react-redux";
 import { Creators as HistoryCreator } from "../../store/ducks/history";
+import { Table, Button, Container, Col, Row, Form } from "react-bootstrap";
 
 class History extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      orderToRate: -1,
+    };
+  }
+
   componentDidMount() {
     this.props.getHistory();
   }
 
   render() {
     const { data } = this.props.history;
-    return <h1>oi</h1>;
+    const { orderToRate } = this.state;
+    return (
+      <PageStyle>
+        <BorderText className="m-3">
+          <h1>Histórico de pedidos</h1>
+        </BorderText>
+        <MainDiv>
+          <Table borderless variant="info">
+            <TableStyle>
+              <tbody>
+                {data.map((element) => (
+                  <tr key={element.id}>
+                    <td className="p-2">
+                      <TableBodyStyle>
+                        <p>Pedido {element.id + 1}</p>
+                        <p>Preço total: {element.total_price}</p>
+                      </TableBodyStyle>
+                    </td>
+                    <td>
+                      <Button
+                        variant="primary"
+                        onClick={() => this.setState({ orderToRate: 0 })}
+                        className="mx-3"
+                      >
+                        Avaliar Pedido
+                      </Button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </TableStyle>
+          </Table>
+          {orderToRate > -1 ? (
+            <Form className="m-3">
+              <Form.Group controlId="userFeedback">
+                <Form.Label>
+                  <RateLabel>
+                    <h2>Pedido {data[orderToRate].id + 1}</h2>
+                    <Button variant="warning">Detalhes</Button>
+                  </RateLabel>
+                </Form.Label>
+                <Form.Control
+                  as="textarea"
+                  type="text"
+                  className="mr-2"
+                  rows={12}
+                  placeholder="Deixe seu feeback!"
+                />
+                <Form.Text className="text-muted">
+                  Sua avaliação nos ajuda a melhorar a experiência do app :{")"}
+                </Form.Text>
+              </Form.Group>
+              <ActionButtonsStyle className="mt-2">
+                <Button
+                  variant="danger"
+                  onClick={() => this.setState({ orderToRate: -1 })}
+                >
+                  Cancelar
+                </Button>
+                <Button variant="success">Enviar</Button>
+              </ActionButtonsStyle>
+            </Form>
+          ) : null}
+        </MainDiv>
+      </PageStyle>
+    );
   }
 }
 
---
+++ client/src/components/History/styles.js
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+
+export const ActionButtonsStyle = styled.div`
+  display: grid;
+  grid-auto-flow: column;
+  grid-column-gap: 2vh;
+`;
+
+export const RateLabel = styled.div`
+  display: grid;
+  grid-auto-flow: column;
+  grid-column-gap: 2vh;
+  font-size: 40px;
+  line-height: 47px;
+  width: 100%;
+`;
+
+export const MainDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+export const TableStyle = styled.div`
+  width: 100%;
+`;
+
+export const TableBodyStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+export const PageStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const BorderText = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 35%;
+  color: white;
+  line-height: 70px;
+  font-weight: 400px;
+  text-align: center;
+  background: rgba(250, 16, 52, 0.82);
+  border-radius: 63px;
+`;
