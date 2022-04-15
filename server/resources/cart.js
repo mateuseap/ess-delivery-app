49b474fc8ca47409bf3ec37fc486d79af9b8c0e2
Enrique <elaborao@hotmail.com>
2022-04-15 20:45:58 -0300

    [SERVER] Initial Cart implementation

--- server/resources/cart.js
+++ server/resources/cart.js
@@ -1,3 +1,21 @@
-exports.getCart = async (req, res) => {};
+const { ManipulateDatabase } = require("../utils/db");
+const { getRandomSlice } = require("../utils/misc");
 
-exports.postCart = async (req, res) => {};
+const table = new ManipulateDatabase("carts");
+
+exports.getCart = async (req, res) => {
+  try {
+    res.status(200).send(JSON.stringify(table.getArray()[0]));
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
+
+exports.postCart = async (req, res) => {
+  try {
+    table.write({ orders: req.body.data });
+    res.status(200).send(JSON.stringify(table.getArray()));
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
