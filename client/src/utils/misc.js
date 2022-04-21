7f3541944c4ff9fcf65dbe375f8af5973789bc53
Enrique <elaborao@hotmail.com>
2022-04-21 17:37:00 -0300

    [CLIENT] Added WithRoute function

--- client/src/utils/misc.js
+++ client/src/utils/misc.js
@@ -1,3 +1,5 @@
+import { useLocation, useNavigate, useParams } from "react-router-dom";
+
 export function formatMoney(
   amount,
   decimalCount = 2,
@@ -29,4 +31,15 @@ export function formatMoney(
   } catch (e) {
     console.log(e);
   }
+}
+
+export function withRouter(Component) {
+  function ComponentWithRouterProp(props) {
+    let location = useLocation();
+    let navigate = useNavigate();
+    let params = useParams();
+    return <Component {...props} router={{ location, navigate, params }} />;
+  }
+
+  return ComponentWithRouterProp;
 }
