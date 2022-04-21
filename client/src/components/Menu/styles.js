08d46d90b80efe0d1aa72b2d62b72e90a511cbf7
Williams Santiago <wssf@cin.ufpe.br>
2022-04-21 16:21:08 -0300

    [CLIENT] styles modificado na página de Menu

--- client/src/components/Menu/styles.js
+++ client/src/components/Menu/styles.js
@@ -7,9 +7,34 @@ export const PageStyle = styled.div`
   align-items: center;
 `;
 
+export const MenuStyle = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const MenuItemStyle = styled.div`
+  display: flex;
+  justify-content: center;
+  max-width: 350px;
+  align-items: center;
+  margin: auto;
+`;
+
 export const ItemPhoto = styled.img`
   content: url(${(props) => props.photo});
+  height: 50%;
+  border-radius: 65px;
+`;
+
+export const ItemData = styled.div`
+  display: flex;
+  flex-direction: column;
   margin: auto;
-  width: 110px;
-  border-radius: 50%;
+  padding: 1em;
+`;
+
+export const ItemDescriptionStyle = styled.p`
+  font-size: 18px;
+  font-weight: 600;
 `;
